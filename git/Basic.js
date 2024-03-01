d894f99797673eb810a859c6cf866f24921681c3
Sushant <shekahrsushant301@gmail.com>
2024-03-01 22:45:00 +0530

    force

---
+++ git/Basic.js
@@ -0,0 +1,26 @@
+//java script is client side scripting language
+// arounf 98.8% website of the world use java script for tehir client side programming language
+//this is one of the most popular languagee in the world
+// Java script and java are two diffrent programming language
+
+//javascript is dynamic programming lanuage
+
+
+// int a = 20;
+// there are three keyword in javascript that used to creating a variable
+// unlike java javascript does not need any type declaration
+
+// here a is variable anme 
+// var is keyword
+// after equal 20 is value which a store 
+
+var a=20;
+// this console will be visible either in the terminal or browser conaole
+console.log(a);
+
+// javascript sconsider sinle quote and double quote as a sring only
+
+var name="vishal";
+var lastname="sharma";
+
+console.log('Name=', name, lastname);
